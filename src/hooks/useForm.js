0a76d4ace73ca92c97f0be6cdbe6590ac0a9bc37
Daniil Borovov <borovovdaniil@yandex.ru>
2023-05-10 16:17:16 +0600

    add custom hook for form state

---
+++ src/hooks/useForm.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function useForm(inputValues) {
+  const [values, setValues] = useState(inputValues)
+
+  const handleChange = event => {
+    const { value, name } = event.target
+    setValues({ ...values, [name]: value })
+  }
+  return { values, handleChange, setValues }
+}
+
+export default useForm
