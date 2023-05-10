950f8525cb97396f5d5b7d526330b3b2a82a9a3b
Daniil Borovov <borovovdaniil@yandex.ru>
2023-05-10 14:56:21 +0600

    fix: undefined state error

--- src/components/EditProfilePopup.jsx
+++ src/components/EditProfilePopup.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import PopupWithForm from './PopupWithForm';
-import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import React from 'react'
+import PopupWithForm from './PopupWithForm'
+import { CurrentUserContext } from '../contexts/CurrentUserContext'
 
 function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
-  const [name, setName] = React.useState('');
-  const [description, setDescription] = React.useState('');
-  const currentUser = React.useContext(CurrentUserContext);
+  const [name, setName] = React.useState('')
+  const [description, setDescription] = React.useState('')
+  const currentUser = React.useContext(CurrentUserContext)
 
   React.useEffect(() => {
     if (currentUser) {
-      setName(currentUser.name);
-      setDescription(currentUser.about);
+      setName(currentUser.name)
+      setDescription(currentUser.about)
     }
-  }, [currentUser, isOpen]);
+  }, [currentUser, isOpen])
 
   function handleNameChange(e) {
-    setName(e.target.value);
+    setName(e.target.value)
   }
 
   function handleDescriptionChange(e) {
-    setDescription(e.target.value);
+    setDescription(e.target.value)
   }
 
   function handleSubmit(e) {
     // Запрещаем браузеру переходить по адресу формы
-    e.preventDefault();
+    e.preventDefault()
 
     // Передаём значения управляемых компонентов во внешний обработчик
     onUpdateUser({
       name: name,
       about: description,
-    });
+    })
   }
 
   return (
@@ -51,7 +51,7 @@ function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
           required
           minLength='2'
           maxLength='40'
-          value={name}
+          value={name || ''}
           onChange={handleNameChange}
         />
         <span className='edit-form__error' />
@@ -65,13 +65,13 @@ function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
           required
           minLength='2'
           maxLength='200'
-          value={description}
+          value={description || ''}
           onChange={handleDescriptionChange}
         />
         <span className='edit-form__error' />
       </div>
     </PopupWithForm>
-  );
+  )
 }
 
-export default EditProfilePopup;
+export default EditProfilePopup
